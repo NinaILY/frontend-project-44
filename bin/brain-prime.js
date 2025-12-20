4b8ddfb0162b65361efaa2881d8ec8634706d017
Nina <user@Asusik.localdomain>
2025-12-20 20:27:39 +0300

    Update5

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,4 +3,4 @@
 import { answerUser } from '../src/index.js'
 import isPrimeNum from '../games/primenum.js'
 
-answerUser(isPrimeNum, `Answer "yes" if the number is even, otherwise answer "no".`)
+answerUser(isPrimeNum, `Answer "yes" if given number is prime. Otherwise answer "no".`)
